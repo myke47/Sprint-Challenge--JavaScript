f211fb91690a3263bf772c11b45cdf606f7b0dca
Myke47 <mychel.larson@gmail.com>
2019-04-05 20:26:39 -0700

    I'm lagging way behind

--- challenges/functions.js
+++ challenges/functions.js
@@ -13,10 +13,13 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(x, y, cb) {
+  return cb(consume(x+y));
+};
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
+consume(2,2,add); // 4
 // consume(10,16,multiply); // 160
 // consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,6 +5,13 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
+function CuboidMaker(cbProps) {
+  this.length = cbProps.length;
+  this.width = cbProps.width;
+  this.height = cbProps.height; 
+  this.volume = cbProps.volume;
+}
+
 
 
 /* == Step 2: Volume Method ==
@@ -12,6 +19,9 @@
   
   Formula for cuboid volume: length * width * height
 */
+CuboidMaker.prototype.volume = function () {
+  this.length * this.width * this.height;
+}
 
 
 /* == Step 3: Surface Area Method ==
@@ -25,9 +35,17 @@
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
+const cuboid = new CuboidMaker ({
+  cbProps: {
+    length: 4,
+    width: 5,
+    height: 5
+}
+
+});
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
+console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
 
 
